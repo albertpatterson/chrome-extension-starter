252faf3e4f16336ad24f2d998f8a4230b4ae1b61
albert patterson <apatterson189@gmail.com>
2022-05-23 20:43:54 -0500

    add hashbang

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { runTasks } from './run_tasks.js';
 import { get_settings } from './get_settings.js';
 
@@ -5,5 +7,5 @@ try {
   const settings = await get_settings(process.argv);
   await runTasks(settings);
 } catch (err) {
-  console.log(err.message);
+  console.error(err.message);
 }
