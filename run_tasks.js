71618101c8c2a3cbbbd60a19ebd245f7ef41105d
albert patterson <apatterson189@gmail.com>
2022-06-04 12:12:39 -0500

    add license message

--- run_tasks.js
+++ run_tasks.js
@@ -1,3 +1,19 @@
+/**
+ * @file
+ * @author Albert Patterson <albert.patterson.code@gmail.com>
+ * @see [Linkedin]{@link https://www.linkedin.com/in/apattersoncmu/}
+ * @see [Github]{@link https://github.com/albertpatterson}
+ * @see [npm]{@link https://www.npmjs.com/~apatterson189}
+ * @see [Youtube]{@link https://www.youtube.com/channel/UCrECEffgWKBMCvn5tar9bYw}
+ * @see [Medium]{@link https://medium.com/@albert.patterson.code}
+ *
+ * Free software under the GPLv3 licence. Permissions of this strong copyleft
+ * license are conditioned on making available complete source code of
+ * licensed works and modifications, which include larger works using a
+ * licensed work, under the same license. Copyright and license notices must
+ * be preserved. Contributors provide an express grant of patent rights.
+ */
+
 import fs from 'fs';
 import path from 'path';
 import { Listr } from 'listr2';
@@ -27,14 +43,13 @@ async function setup(writeDir) {
 }
 
 async function copyCommonTemplates(writeDir) {
-  for (const file of [
-    ['babel.config.cjs'],
-    ['jest.config.cjs'],
-    ['package.json'],
-    ['tsconfig.json'],
-    ['README.md'],
-    ['src', 'manifest.json'],
-  ]) {
+  const commonFiles = await (
+    await tools.getFiles(TEMPLATE_DIR)
+  ).map((f) => [f]);
+
+  const allFiles = [...commonFiles, ['src', 'manifest.json']];
+
+  for (const file of allFiles) {
     const template = path.resolve(TEMPLATE_DIR, ...file);
     const out = path.resolve(writeDir, ...file);
 
